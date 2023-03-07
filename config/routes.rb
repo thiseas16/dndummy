90b9cb0734e26a19888130c1fa58aa134cde5252
Gabriel Mestre <babygogeta7000@gmail.com>
2023-03-07 19:00:46 +0000

    routes done to the best of my knowledge

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :dices, only: %i[new create show]
+  resources :characters, only: %i[index new create show edit update destroy]
+  resources :handbooks, only: %i[index]
+  end
+  resources :encounters, only: %i[index new]
+  resources :campaigns, only: %i[create show]
+  resources :random, only: %i[new]
 end
