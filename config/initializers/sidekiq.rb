f253d804374a238e238c8132e58b03010601fbb2
Thiseas Boulakis <thiseas16@gmail.com>
2023-03-31 16:56:26 +0200

    renamed wrong file name and fixed fetch method

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,15 @@
 Sidekiq.configure_server do |config|
   config.redis = {
-    url: ENV["REDISCLOUD_URL"],
-    password: ENV["REDIS_PASSWORD"],
+    url: ENV.fetch("REDISCLOUD_URL"),
+    password: ENV.fetch("REDIS_PASSWORD"),
     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    url: ENV["REDISCLOUD_URL"],
-    password: ENV["REDIS_PASSWORD"],
+    url: ENV.fetch("REDISCLOUD_URL"),
+    password: ENV.fetch("REDIS_PASSWORD"),
     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   }
 end
