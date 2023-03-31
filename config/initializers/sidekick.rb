071921758567cc8002081a6248e523f447f249bf
Thiseas Boulakis <thiseas16@gmail.com>
2023-03-31 16:44:58 +0200

    sidekiq initializer

---
+++ config/initializers/sidekick.rb
@@ -0,0 +1,15 @@
+Sidekiq.configure_server do |config|
+  config.redis = {
+    url: ENV.fetch["REDIS_URL"],
+    password: ENV.fetch["REDIS_PASSWORD"],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = {
+    url: ENV.fetch["REDIS_URL"],
+    password: ENV.fetch["REDIS_PASSWORD"],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+end
